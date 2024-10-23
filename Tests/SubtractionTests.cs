ea0a971a5bc10fabd8377ca990a375add6ee6e7c
Ilya <illua.komolov@gmail.com>
2024-10-23 22:01:02 +0300

    Added tests for the Subtraction class

---
+++ Tests/SubtractionTests.cs
@@ -0,0 +1,23 @@
+﻿using ClassLibrary;
+
+namespace Tests
+{
+    internal class SubtractionTests
+    {
+        private Subtraction _subtraction;
+
+        [SetUp]
+        public void Setup()
+        {
+            _subtraction = new Subtraction();
+        }
+
+        [Test]
+        public void Subtract_Should_Return_Correct_Difference()
+        {
+            double result = _subtraction.Subtract(5, 6);
+
+            Assert.That(result, Is.EqualTo(-1));
+        }
+    }
+}
