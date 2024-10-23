fbfbf7a80ea845d8751db6395b7a5547bcb5bc6f
Ilya <illua.komolov@gmail.com>
2024-10-23 22:04:08 +0300

    Added tests for the Multiplication class

---
+++ Tests/MultiplicationTests.cs
@@ -0,0 +1,28 @@
+﻿using ClassLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tests
+{
+    internal class MultiplicationTests
+    {
+        private Multiplication _multiplication;
+
+        [SetUp]
+        public void Setup()
+        {
+            _multiplication = new Multiplication();
+        }
+
+        [Test]
+        public void Multiply_Should_Return_Correct_Product()
+        {
+            double result = _multiplication.Multiply(5, 6);
+
+            Assert.That(result, Is.EqualTo(30));
+        }
+    }
+}
